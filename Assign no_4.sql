78e1b85235f1799110bcd01d57f2a9a6f29ba3ae
shubhammahajan308 <65168569+shubhammahajan308@users.noreply.github.com>
2020-07-17 23:00:10 +0530

    Add files via upload

---
+++ Assign no_4.sql
@@ -0,0 +1,17 @@
+select Onum,amt,odate from orders;
+
+
+select* from customers 
+where snum=1001;
+
+select city,sname,snum,comm  from salespeople;
+
+select rating,cname from customers 
+where city="San Jose";
+
+Select * from salespeople;
+
+select distinct snum 
+from orders 
+order by onum ;
+
